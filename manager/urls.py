d7244d0db2ff9830ca023018dda2b20d8721a291
BetterLuckNextLife <rykovegor020@gmail.com>
2024-12-13 23:55:44 +0300

    Обновлены urls.py и views.py

--- manager/urls.py
+++ manager/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('help/', views.help, name="help"),
+    path('register/', views.register, name="register"),
+    path('user/', views.user, name="user"),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'base.html', {})
+    return render(request, 'index.html', {})
+
+def help(request):
+    return render(request, 'help.html', {})
+
+def register(request):
+    return render(request, 'register.html', {})
+
+def user(request):
+    return render(request, 'user.html', {})
+
