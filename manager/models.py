f28836e03263037eedf2a4b0f21899da90b1dbaf
BetterLuckNextLife <rykovegor020@gmail.com>
2024-12-13 20:15:36 +0300

    Созданы модели User и Product

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    patronymic = models.CharField(max_length=64, blank=True, null=True)
+    password_hash = models.CharField(max_length=128)
+    verified = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.username} ({self.name} {self.surname})"
+
+class Product(models.Model):
+    STATE_CHOICES = [
+        ('active', 'Используется'),
+        ('inactive', 'В запасе'),
+        ('broken', 'Сломан'),
+    ]
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='products', blank=True, null=True)
+    state = models.CharField(max_length=20, choices=STATE_CHOICES, default='inactive')
+
+    def __str__(self):
+        return f"{self.name} ({self.get_state_display()})"
