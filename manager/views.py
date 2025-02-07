af9d5a160386c5367638983a473dfc65ea7d1f79
_void <fejkfejkov25@gmail.com>
2025-02-07 20:56:13 +0300

    Исправлена ошибка с подтверждением пользователей

--- manager/views.py
+++ manager/views.py
@@ -33,8 +33,8 @@ def error_404_view(request, exception=None):
 @user_passes_test(is_admin)
 def admin_panel(request):
     if request.method == "POST":
-        if "verifingusername" in request.POST:
-            username = request.POST.get('verifingusername')
+        if "username" in request.POST:
+            username = request.POST.get('username')
             verifinguser = User.objects.filter(username=username).first()
             verifinguser.verified = True
             verifinguser.save()
